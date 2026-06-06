9d9666c85fd074661fdc35b8f778514e04139e01
ngquanghuy206 <ngquanghuy3027@gmail.com>
2026-06-07 04:13:27 +0700

    Add files via upload

--- nhac.js
+++ nhac.js
@@ -187,7 +187,7 @@ function pgNhac(){
       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="15 18 9 12 15 6"/></svg>
     </button>
 
-  </div>`</div>`;
+  </div>`;
 
   setupNavScroll();
 
